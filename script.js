4e0f2d22c46be42ef07804b4f16cfd62956784ae
yogiksuryatmika19 <yogiksuryatmika@gmail.com>
2026-03-10 20:24:00 +0800

    finshing file script.js dan fixing bug

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function openModal() {
 function closeModal() {
     modal.classList.remove("show");
 }
+
+// menambah event klik untuk open modal
+openBtn.addEventListener("click", openModal);
+
+// menambah event klik untuk close modal
+closeBtn.addEventListener("click", closeModal);
+
+// menambah event untuk melakukan close modal ketika melakukan klik diluar modal box
+modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+//  menambah event untuk melakukan close modal ketika tombol esc diklik
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+        closeModal();
+    }
+});
